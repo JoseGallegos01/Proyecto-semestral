52fad6fc93c50266daf3e5532f954a9e84086799
Vicente <vicente.salinas2501@alumnos.ubiobio.cl>
2026-04-07 16:50:07 -0400

    Avances en clases main, bus y viaje

---
+++ src/src/Viaje.java
@@ -0,0 +1,51 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Viaje {
+    //Vicente Salinas
+
+    private LocalDate fecha;
+    private LocalTime hora;
+    private int precio;
+    private Bus bus;
+
+    public Viaje(LocalDate fecha, LocalTime hora, int precio, Bus bus){
+        this.fecha = fecha;
+        this.hora = hora;
+        this.precio = precio;
+        this.bus = bus;
+    }
+    public LocalDate getFecha() {
+        return fecha;
+    }
+    public LocalTime getHora() {
+        return hora;
+    }
+    public int getPrecio() {
+        return precio;
+    }
+    public void setPrecio(){
+        this.precio = precio;
+    }
+    public Bus getBus(){
+        return bus;
+    }
+    public void setBus(Bus bus){
+        this.bus = bus;
+    }
+
+    //AVANZAR UNA VEZ LA CLASE PASAJE ESTE LISTA
+
+    //public String[][] getAsientos() {}
+        //no se puede avanzar sin la clase Pasaje
+    //public String[][] getListaPasajeros() {}
+        //no se puede avanzar sin la clase Pasaje
+    public boolean existeDisponibilidad() {
+        //no se puede avanzar sin la clase Pasaje
+        return false;
+    }
+    public int getNroAsientosDisponibles() {
+        //no se puede avanzar sin la clase Pasaje
+        return 0;
+    }
+}
