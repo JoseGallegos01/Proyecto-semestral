cf9a5640d6978b35af5f9a7114841146788b50e1
Vicente <vicente.salinas2501@alumnos.ubiobio.cl>
2026-04-25 19:22:40 -0400

    Avances en el menu en el main y de metodos en SistemaVentaPasajes

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -53,15 +54,54 @@ public class Main {
     private void createCliente(){
         System.out.println("...::::Crear un nuevo cliente:::...");
         System.out.println("Rut[1] o Pasaporte[2]");
+        int opcionRutPasaporte = sc.nextInt();
+        IdPersona id;
+        if (opcionRutPasaporte==1){
+            System.out.println("Ingrese el rut (sin el DV)");
+            int rut = sc.nextInt();
+            System.out.println("Ingrese el DV del rut");
+            char dv = sc.next().charAt(0);
+            id = new Rut(rut, dv);
+        }
+        else if (opcionRutPasaporte==2){
+            System.out.println("Ingrese el numero del pasaporte");
+            String numero = sc.nextLine();
+            System.out.println("Ingrese la nacionalidad del pasaporte");
+            String nacionalidad = sc.nextLine();
+            id = new Pasaporte(numero, nacionalidad);
+        }
         System.out.println("Sr. [1] o Sra. [2]");
+        int opcionSrSra = sc.nextInt();
+        Tratamiento tratamiento;
+        if (opcionSrSra==1)tratamiento = Tratamiento.SR;
+        if (opcionSrSra==2)tratamiento = Tratamiento.SRA;
         System.out.println("Nombres: ");
+        String nombres = sc.next();
         System.out.println("Apellido paterno: ");
+        String apellido_paterno = sc.next();
         System.out.println("Apellido materno: ");
+        String apellido_materno = sc.next();
         System.out.println("Telefono movil: ");
+        int telefono_movil = sc.nextInt();
         System.out.println("Email: ");
+        String email = sc.next();
     }
     private void createBus(){
-
+        System.out.println("...::::Creación de un nuevo bus:::...");
+        System.out.println("Patente:");
+        String patente = sc.next();
+        System.out.println("Marca: ");
+        String marca = sc.next();
+        System.out.println("Modelo: ");
+        String modelo = sc.next();
+        System.out.println("Numero de asientos: ");
+        int asientos = sc.nextInt();
+        if (sv.createBus(patente, marca, modelo, asientos)) {
+            System.out.println("...::::Bus guardado exitosamente:::...");
+        }
+        else  {
+            System.out.println("...::::Ya hay un bus con la misma patente registrada::...");
+        }
     }
     private void createViaje(){
 
