1280dd2053467b852b82a9785e9b49ffa972eb1f
JoseGallegos01 <jose.gallegos2301@alumnos.ubiobio.cl>
2026-05-21 01:01:06 -0400

    Se crea clase Tripulante, Conductor y Auxiliar

---
+++ src/src/Conductor.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Conductor extends  Tripulante {
+
+    private List<Viaje> viajes;
+
+    public Conductor(IdPersona id, Nombre nom, Direccion dir) {
+        super(id, nom, dir);
+        this.viajes = new ArrayList<>();
+    }
+
+    public void addViaje(Viaje viaje) {
+        this.viajes.add(viaje);
+    }
+
+    public int getNroVIaje() {
+        return this.viajes.size();
+    }
+}
